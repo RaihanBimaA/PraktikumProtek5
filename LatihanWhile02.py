d5c4538ee15261837b0055945211892ac4a9b749
raihanbimaac <raihanbimaac@gmail.com>
2020-10-23 16:03:02 +0700

    Create LatihanWhile02.py

---
+++ LatihanWhile02.py
@@ -0,0 +1,12 @@
+#Program untuk menampilkan bilangan bulat 0 - 100 yang ganjil
+
+i = 1
+jml = 0
+
+
+while(i <= 100) :
+    print(str(i) + '\n')
+    i = i + 2
+    jml = jml + 1
+
+print('Banyaknya Bilangan Ganjil : ' + str(jml))
