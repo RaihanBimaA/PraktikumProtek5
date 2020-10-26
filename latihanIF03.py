d08306b1d0db45e4c8c1c06c7a3caca94cabdaad
RaihanBimaA <71623625+RaihanBimaA@users.noreply.github.com>
2020-10-26 11:31:28 +0700

    Update latihanIF03.py

--- latihanIF03.py
+++ latihanIF03.py
@@ -4,11 +4,11 @@ ipa=int(input('masukan nilai IPA:'))
 mtk=int(input('masukan nilai mtk:'))
 print('\n')
 
-if(indo<=0 or indo>=100):
+if(indo<0 or indo>100):
     print('Maaf input ada yang tidak valid')
-elif(ipa<=0 or ipa>=100):
+elif(ipa<0 or ipa>100):
     print('Maaf input ada yang tidak valid')
-elif(mtk<=0 or mtk>=100):
+elif(mtk<0 or mtk>100):
     print('Maaf input ada yang tidak valid')
 elif(indo>=60 and ipa>=60 and mtk>70):
     print('LULUS')
